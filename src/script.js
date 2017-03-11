3f98b6c85da48ba4d0bd54cb00fbab333f7b75b6
vivienzi <vivienzi130@gmail.com>
2017-03-11 17:16:26 -0500

    add color

--- src/script.js
+++ src/script.js
@@ -1,8 +1,22 @@
-var viewer = new Cesium.Viewer('cesiumContainer');
-var scene = viewer.scene;
-//state.lon, state.lat state['Call Drop Rate'][1][1]
-//state['Call Drop Rate'][1][0]
-//state.lon, state.lat state['Call Drop Rate'][1][1]
+var viewer = new Cesium.Viewer('cesiumContainer', {
+    imageryProvider : new Cesium.ArcGisMapServerImageryProvider({
+        url : 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer'
+    }),
+    baseLayerPicker : false
+});
+
+Sandcastle.reset = function() {
+  viewer.dataSources.removeAll();
+  //Set the camera to a US centered tilted view and switch back to moving in world coordinates.
+  viewer.camera.lookAt(Cesium.Cartesian3.fromDegrees(-98.0, 40.0), new Cesium.Cartesian3(0.0, -4790000.0, 3930000.0));
+  viewer.camera.lookAtTransform(Cesium.Matrix4.IDENTITY);
+};
+
+Sandcastle.finishedLoading();
+viewer.dataSources.add(Cesium.GeoJsonDataSource.load('ne_10m_us_states.topojson', {
+        stroke: Cesium.Color.PINK,
+        strokeWidth: 3
+    }));
 Cesium.loadJson('nations_test.json').then(function(Data) {
   for( state of Data){
     var blueBox = viewer.entities.add({
@@ -12,6 +26,7 @@ Cesium.loadJson('nations_test.json').then(function(Data) {
     for( i in state['Call Drop Rate']){
       var date = state['Call Drop Rate'][i][0];
       var rate = state['Call Drop Rate'][i][1];
+      var subs = state['Subscriber'][i][1];
       // var clock = new Cesium.Clock({
       //      startTime : Cesium.JulianDate.fromIso8601(state['Call Drop Rate'][0][0]),
       //      currentTime : Cesium.JulianDate.fromIso8601(date),
@@ -19,8 +34,10 @@ Cesium.loadJson('nations_test.json').then(function(Data) {
       //      clockRange : Cesium.ClockRange.CLAMPED,
       // });
       blueBox.box = {
-          dimensions : new Cesium.Cartesian3(200000.0, 150000.0, rate*100000000),
-          material : Cesium.Color.BLUE.withAlpha(0.5),
+          dimensions : new Cesium.Cartesian3(150000.0, subs/10, rate*100000000),
+          material : Cesium.Color.fromRandom({
+            alpha: 1.0
+          }),
           outline : true,
       }
     }
@@ -28,3 +45,8 @@ Cesium.loadJson('nations_test.json').then(function(Data) {
 }).otherwise(function(error) {
     alert("ERROR: ",error);
 });
+
+//
+// state.lon, state.lat state['Call Drop Rate'][1][1]
+// state['Call Drop Rate'][1][0]
+// state.lon, state.lat state['Call Drop Rate'][1][1]
